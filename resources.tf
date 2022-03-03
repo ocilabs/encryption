2ac7e11649e35ec8539069828f5263196788498b
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-03 17:50:57 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -37,6 +37,7 @@ resource "oci_kms_verify" "wallet" {
   depends_on        = [oci_kms_vault.wallet, oci_kms_key.wallet, oci_kms_sign.wallet]
   crypto_endpoint   = oci_kms_vault.wallet.crypto_endpoint
   key_id            = oci_kms_key.wallet.id
+  key_version_id    = data.oci_kms_key_versions.wallet.key_versions[0].id
   message           = var.encryption.signature.message
   signing_algorithm = var.encryption.signature.algorithm
   signature         = oci_kms_sign.wallet.signature
