17b8fb582a473c89d94bb0b5df59870b6330d5c7
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-09 07:22:01 +0100

    Update signature.tf

--- signature.tf
+++ signature.tf
@@ -7,19 +7,19 @@ resource "oci_kms_sign" "wallet" {
   crypto_endpoint   = oci_kms_vault.wallet.crypto_endpoint
   key_id            = oci_kms_key.wallet.id
   key_version_id    = data.oci_kms_key_versions.wallet.key_versions[0].id
-  message           = each.value.message
   signing_algorithm = each.value.algorithm
+  message           = each.value.message
   message_type      = each.value.type
 }
 
 resource "oci_kms_verify" "wallet" {
   depends_on        = [oci_kms_vault.wallet, oci_kms_key.wallet, oci_kms_sign.wallet]
-  for_each          = var.encryption.signatures
+  for_each          = oci_kms_sign.wallet
   crypto_endpoint   = oci_kms_vault.wallet.crypto_endpoint
   key_id            = oci_kms_key.wallet.id
   key_version_id    = data.oci_kms_key_versions.wallet.key_versions[0].id
-  message           = each.value.message
-  signing_algorithm = each.value.algorithm
+  signing_algorithm = each.value.signing_algorithm
   signature         = each.value.signature
-  message_type      = each.value.type
+  message           = each.value.message
+  message_type      = each.value.message_type
 }
