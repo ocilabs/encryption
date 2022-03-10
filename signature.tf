134bbccb96e7559c834e9988218b0dae9d7d8d3f
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-10 08:19:57 +0100

    Update signature.tf

--- signature.tf
+++ signature.tf
@@ -6,7 +6,7 @@ resource "oci_kms_sign" "wallet" {
   for_each          = var.input.create == true ? var.encryption.signatures  : {}
   crypto_endpoint   = oci_kms_vault.wallet[0].crypto_endpoint
   key_id            = oci_kms_key.wallet[0].id
-  key_version_id    = data.oci_kms_key_versions.wallet.key_versions[0].id
+  key_version_id    = data.oci_kms_key_versions.wallet.key_versions.id
   signing_algorithm = each.value.algorithm
   message           = each.value.message
   message_type      = each.value.type
