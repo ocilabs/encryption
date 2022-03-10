5c5cf5c02c98ee6575d6050a0aa38dffce9c8a7a
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-10 08:23:42 +0100

    Update signature.tf

--- signature.tf
+++ signature.tf
@@ -1,6 +1,7 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+/*
 resource "oci_kms_sign" "wallet" {
   depends_on        = [oci_kms_vault.wallet, oci_kms_key.wallet]
   for_each          = var.input.create == true ? var.encryption.signatures  : {}
@@ -11,8 +12,6 @@ resource "oci_kms_sign" "wallet" {
   message           = each.value.message
   message_type      = each.value.type
 }
-
-/*
 resource "oci_kms_verify" "wallet" {
   depends_on        = [oci_kms_vault.wallet, oci_kms_key.wallet, oci_kms_sign.wallet]
   for_each          = oci_kms_sign.wallet
