2d2a1512ba856f63b462ad51592b18c806d24491
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-10 22:09:24 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -32,6 +32,5 @@ output "secret_id" {
 }
 
 output "secret_content" {
-  #value = {for secret in data.oci_secrets_secretbundle.wallet: secret.version_name => secret.secret_bundle_content}
   value = {for secret in data.oci_secrets_secretbundle.wallet: secret.version_name => base64decode(secret.secret_bundle_content.0.content)}
 }
