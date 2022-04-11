fa6af53760f52dd7cc7ae1531f2ff8f0830855c7
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-11 12:53:09 +0200

    Update config.tf

--- config.tf
+++ config.tf
@@ -33,7 +33,7 @@ data "oci_vault_secrets" "wallet" {
   count          = local.wallet_count
   compartment_id = data.oci_identity_compartments.security.compartments[0].id
   state          = "ACTIVE"
-  vault_id       = oci_kms_vaults.wallet[count.index].id
+  vault_id       = oci_kms_vault.wallet[count.index].id
 }
 
 data "oci_kms_key_versions" "wallet" {
