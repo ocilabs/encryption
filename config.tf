fe239aeaf753b625a70de4ca79172c0f9d016cd1
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-11 12:03:18 +0200

    find vaults

--- config.tf
+++ config.tf
@@ -19,9 +19,16 @@ data "oci_identity_compartments" "security" {
   state          = "ACTIVE"
 }
 
+data "oci_kms_vaults" "wallet" {
+  depends_on          = [
+    data.oci_identity_compartments.security
+  ]
+  compartment_id = data.oci_identity_compartments.security.compartments[0].id
+}
+
 data "oci_vault_secrets" "wallet" {
   depends_on     = [
-    oci_kms_vault.wallet
+    data.oci_kms_vaults.wallet
   ]
   count          = local.wallet_count
   compartment_id = data.oci_identity_compartments.security.compartments[0].id
@@ -49,7 +56,7 @@ data "oci_secrets_secretbundle" "wallet" {
 locals {
   wallet_count = var.schema.create ? 1 : 0
   secret_map   = {for secret in oci_vault_secret.wallet : secret.secret_name => secret.id}
-  existing_secrets = [for index in local.wallet_count: length(data.oci_vault_secrets.wallet) > 0 ? data.oci_vault_secrets.wallet[index].secrets[*].secret_name : null]
+  #existing_secrets = [for index in local.wallet_count: length(data.oci_vault_secrets.wallet) > 0 ? data.oci_vault_secrets.wallet[index].secrets[*].secret_name : null]
 }
 
 // Define the wait state for the data requests
--- output.tf
+++ output.tf
@@ -1,8 +1,14 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+/*
 output "existing_secrets" {
   value = local.existing_secrets
+}
+*/
+
+output "existing_wallets" {
+  value = data.oci_kms_vaults.wallet
 }
 
 output "compartment_id" {
