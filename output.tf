6a5cd29ddea4b4e488b50103d1ec69fee991bfc5
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-11 07:30:16 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 output "existing_secrets" {
-  value = data.oci_vault_secrets.wallet[0].secrets
+  value = length(data.oci_vault_secrets.wallet) > 0 ? data.oci_vault_secrets.wallet[0].secrets[0].secret_name : null
 }
 
 output "compartment_id" {
